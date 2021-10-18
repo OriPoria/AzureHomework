9a491ab735d9859a1f8d40b9660828b1ad40a44a
OriPoria <58737667+OriPoria@users.noreply.github.com>
2021-10-18 19:54:38 +0300

    final yml file for deploy

--- create-script.ps1
+++
@@ -1,8 +0,0 @@
-# create resource group:
-az group create --name orires --location eastus
-
-# deploy resources from json files:
-az deployment group create --resource-group orires --template-file res.json --parameters res.parameters.json
-
-# delete resource group:
-# az group delete --name devops 
