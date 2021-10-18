234d9d733ca75106d37b661e4d418ac5406681f2
OriPoria <58737667+OriPoria@users.noreply.github.com>
2021-10-18 18:03:49 +0300

    all commands in blobtransfer script

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # AzureHomework
 
+create-script- create the resource group and then apply the ARM file with 2 storages and a VM.
+
 vm-blobtransfer-script- script that runs on the VM through the "RunPowerShellScript" command.
 In this script we initialize the azure cli and then transfer the container from storate 1 to the
 container in stoarget 2.
+
+## How to use?
+After subscribe to your account in vsCode, run create-script.ps1 to create the resources and 
+then run data-transfer-script.ps1 to create the demo blobs, upload to the container in the 
+first storage 
 
--- vm-blobtransfer-script.ps1
+++ vm-blobtransfer-script.ps1
@@ -1,5 +1,24 @@
 Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
-az version
+
+# create 100 files- demo of blobs
+New-Item -Path . -Name "demofile.txt" -ItemType "file" -Value "This is a text string."
+New-item ./files -itemtype directory
+( 1 .. 100 ) | % { Copy-Item .\demofile.txt files\testfile$_.txt }
+
+# create containers in a storages:
+az storage container create --account-name "oriporia1" --name "container1" `
+ --account-key "kcZ/F7n2ik2Ky/CO3Uq+/R6MmOibBygcichEnkYC6XM11ur8T9HytNUMuPfvpkOVL3IhNFeg2J+S+jqBFULb+g==" `
+ --auth-mode key
+az storage container create --account-name "oriporia2" --name "container2" `
+--account-key "aH8vwTzpt9t8EKz7tjP0QjnUxNtiwO581oHn6OHzu1/bXJnPV8PAzbY1adPSK09HA/nwSEvZoSPJt7FxMHKx0Q==" `
+--auth-mode key
+
+# copy all demo files to container1 in storage A:
+az storage blob upload-batch -d "container1" --account-name "oriporia1" -s "./files" `
+    --account-key "kcZ/F7n2ik2Ky/CO3Uq+/R6MmOibBygcichEnkYC6XM11ur8T9HytNUMuPfvpkOVL3IhNFeg2J+S+jqBFULb+g==" `
+    --auth-mode key
+
+# copy blobs from container1 in storage A to container2 in storage B:
 az storage blob copy start-batch `
   --destination-container container2 `
   --account-name oriporia2 `
