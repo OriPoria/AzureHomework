361306256f9f4483897e90f20c0556f2f9f222c7
OriPoria <58737667+OriPoria@users.noreply.github.com>
2021-10-18 17:31:38 +0300

    test gitignore

--- reset.ps1
+++ reset.ps1
@@ -3,4 +3,4 @@ Remove-Item './files'
 az storage container delete --account-name "poriastore1" --name "container1" --auth-mode login
 az storage container delete --account-name "poriastore2" --name "container2" --auth-mode login
 
-#cdcdscds
+#xsa
