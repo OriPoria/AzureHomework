9b9b69a852d792842fc6b8bb051bbf565f0fa9cd
OriPoria <58737667+OriPoria@users.noreply.github.com>
2021-10-18 19:44:22 +0300

    main script edit

--- main-script.ps1
+++ main-script.ps1
@@ -1,13 +1,3 @@
-# # create 100 files- demo of blobs
-# new-item ./files -itemtype directory
-# ( 1 .. 100 ) | % { Copy-Item .\demofile.txt files\testfile$_.txt }
-# # create containers in a storage
-# az storage container create --account-name "oriporia1" --name "container1" --auth-mode login
-# az storage container create --account-name "oriporia2" --name "container2" --auth-mode login
-
-# # copy all demo files to container1 in storage A:
-# az storage blob upload-batch -d "container1" --account-name "oriporia1" -s "./files" --auth-mode key
-
 # run shellscript on my "ori-vm"
 az vm run-command invoke  --command-id RunPowerShellScript --name ori-vm -g orires `
     --scripts @vm-blobtransfer-script.ps1
